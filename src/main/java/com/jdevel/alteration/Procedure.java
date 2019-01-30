58756c6ca8be17a6ced0f2e49b7d09bc2ff210d6
jonathan <jonathan.lee.devel@gmail.com>
2019-01-30 19:25:52 +0000

    Added logic to Procedure & ProcedureContent classes

--- src/main/java/com/jdevel/alteration/Procedure.java
+++ src/main/java/com/jdevel/alteration/Procedure.java
@@ -1,6 +1,7 @@
 package com.jdevel.alteration;
 
 import java.util.ArrayList;
+import java.util.ListIterator;
 
 public class Procedure {
 
@@ -16,8 +17,46 @@ public class Procedure {
         this.contents = new ArrayList<>();
     }
 
-    public ArrayList<ProcedureContent> getContents() {
+    /*
+     * List Access Methods
+     */
+
+    /**
+     * Adds content to contents list
+     * @param content content to be added
+     * @return success flag
+     */
+    public boolean addContent(ProcedureContent content) {
+        return this.getContents().add(content);
+    }
+
+    /**
+     * Removes content from contents list
+     * @param content content to be removed
+     * @return success flag
+     */
+    public boolean removeContent(ProcedureContent content) {
+        return this.getContents().remove(content);
+    }
+
+    /**
+     * Getter for ListIterator for contents list
+     * @return iterator for contents list
+     */
+    public ListIterator<ProcedureContent> getContentsIterator() {
+        return this.getContents().listIterator();
+    }
+
+    /*
+     * Getters and Setters
+     */
+
+    private ArrayList<ProcedureContent> getContents() {
         return this.contents;
+    }
+
+    private void setContents(ArrayList<ProcedureContent> contents) {
+        this.contents = contents;
     }
 
 }
--- src/main/java/com/jdevel/alteration/ProcedureContent.java
+++ src/main/java/com/jdevel/alteration/ProcedureContent.java
@@ -1,6 +1,7 @@
 package com.jdevel.alteration;
 
 import java.util.ArrayList;
+import java.util.ListIterator;
 
 /**
  * Actual content for the procedure, consisting of a header,
@@ -18,31 +19,77 @@ public class ProcedureContent {
      */
     private ArrayList<String> bodies;
 
+    /**
+     * Default Constructor, simply initializes members to non-null values
+     */
     public ProcedureContent() {
-
+        this.header = "";
+        this.bodies = new ArrayList<>();
     }
 
-    public String getHeader() {
-        return this.header;
-    }
+    /*
+     * List Access Methods
+     */
 
-    public void setHeader(String header) {
-        this.header = header;
-    }
-
+    /**
+     * Adds body to bodies list
+     * @param body body to be added
+     * @return success flag
+     */
     public boolean addBody(String body) {
         return this.bodies.add(body);
     }
 
+    /**
+     * Removes body from bodies list
+     * @param body body to be removed
+     * @return success flag
+     */
     public boolean removeBody(String body) {
         return this.bodies.remove(body);
     }
 
-    public ArrayList<String> getBodies() {
+    /**
+     * Getter for ListIterator for bodies ArrayList
+     * @return ListIterator for bodies ArrayList
+     */
+    public ListIterator<String> getBodiesIterator() {
+        return this.getBodies().listIterator();
+    }
+
+    /*
+     * Getters and Setters
+     */
+
+    /**
+     * Getter for the header
+     * @return the header
+     */
+    public String getHeader() {
+        return this.header;
+    }
+
+    /**
+     * Setter for the header
+     * @param header header to be set
+     */
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    /**
+     * Getter for bodies ArrayList
+     * @return bodies ArrayList
+     */
+    private ArrayList<String> getBodies() {
         return this.bodies;
     }
 
-    public void setBodies(ArrayList<String> bodies) {
+    /**
+     * Setter for bodies ArrayList
+     * @param bodies bodies ArrayList to be set
+     */
+    private void setBodies(ArrayList<String> bodies) {
         this.bodies = bodies;
     }
 
