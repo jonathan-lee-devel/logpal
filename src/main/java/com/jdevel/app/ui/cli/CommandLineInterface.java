33beeb42460ab0fe37032e41bc78ae2e6a4e6a92
jonathan <jonathan.lee.devel@gmail.com>
2019-01-31 21:17:56 +0000

    Added CommandLineInterface example usage

--- src/main/java/com/jdevel/app/ui/cli/CommandLineInterface.java
+++ src/main/java/com/jdevel/app/ui/cli/CommandLineInterface.java
@@ -1,6 +1,20 @@
 package com.jdevel.app.ui.cli;
 
+import com.jdevel.alteration.Alteration;
+import com.jdevel.alteration.Procedure;
+import com.jdevel.alteration.ProcedureContent;
+import com.jdevel.alteration.SystemLog;
+import com.jdevel.format.JSONSystemLog;
+import com.jdevel.format.JSONSystemLogGenerator;
+import com.jdevel.format.file.JSONFileWriter;
 import org.apache.commons.cli.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
 
 public class CommandLineInterface {
 
@@ -34,11 +48,44 @@ public class CommandLineInterface {
 
         String inputFilePath = commandLine.getOptionValue("input");
         String outputFilePath = commandLine.getOptionValue("output");
-        String time = commandLine.getOptionValue("time");
+        String inputTime = commandLine.getOptionValue("time");
 
-        System.out.println(inputFilePath);
-        System.out.println(outputFilePath);
-        System.out.println("Time to be set: " + time);
+        LocalDateTime time = (inputTime != null) ? (LocalDateTime.parse(inputTime)) : LocalDateTime.now();
+
+        // Creating example SystemLog
+        SystemLog systemLog = new SystemLog();
+
+        // Example procedure
+        Procedure procedure = new Procedure();
+        ProcedureContent content = new ProcedureContent();
+        content.setHeader("cmd");
+        content.addBody("apt-get install -y git");
+        procedure.addContent(content);
+        // Creating and adding 10 example Alterations to the example SystemLog
+        for (int i = 0; i < 10; i++) {
+            Alteration alteration = new Alteration();
+            alteration.setId(UUID.randomUUID());
+            alteration.setDateTime(time);
+            alteration.setTitle("Title[" + i + "]");
+            alteration.setType("installation");
+            alteration.setProcedure(procedure);
+            systemLog.addAlteration(alteration);
+        }
+
+        // Generating example JSON SystemLog from example SystemLog
+        JSONSystemLogGenerator jsonSystemLogGenerator = new JSONSystemLogGenerator();
+        JSONSystemLog jsonSystemLog = jsonSystemLogGenerator.getFormattedLog(systemLog);
+
+        // Write the log to a .json file
+        File file = new File("/home/jonathan/test." + jsonSystemLog.getFormat().getExtension());
+        JSONFileWriter jsonFileWriter = new JSONFileWriter(file);
+        try {
+            jsonFileWriter.writeJSONSystemLogToFile(jsonSystemLog);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+
+        System.out.println("COMPLETE!!!");
     }
 
 }
